4c3c8d4c532e41cffbe32dbfa7034f4e6dae19bc
Rahul Hembrom <rhembrom21@gmail.com>
2024-06-05 16:48:36 +0530

    Add buffer reader

--- tcp_listener.go
+++ tcp_listener.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 func main() {
@@ -16,6 +18,21 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+
+	for {
+		buf := make([]byte, 1024)
+
+		_, err = conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+
+		conn.Write([]byte("+OK\r\n"))
 	}
 
 	defer conn.Close()
