c92506d189869eba29f394f30dc426cafca111e6
Rahul Hembrom <rhembrom21@gmail.com>
2024-06-22 17:28:47 +0530

    Add readLine and readInteger helper methods in response parser

---
+++ resp.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+const (
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
+)
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+type Value struct {
+	typ   string
+	str   string
+	num   int
+	bulk  string
+	array []Value
+}
+
+func NewResp(reader io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(reader)}
+}
+
+func (r *Resp) Read() (Value, error) {
+	_type, err := r.reader.ReadByte()
+
+	if err != nil {
+		return Value{}, err
+	}
+
+	switch _type {
+	case ARRAY:
+		// return r.readArray()
+		return Value{}, nil
+	case BULK:
+		// return r.readBulk()
+		return Value{}, nil
+	default:
+		fmt.Println("Unknown type: %v", string(_type))
+		return Value{}, nil
+	}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+	return line[:len(line)-2], n, nil
+}
+
+func (r *Resp) readInteger() (x int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err
+	}
+	i64, err := strconv.ParseInt(string(line), 10, 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	return int(i64), n, nil
+}
--- tcp_listener.go
+++ tcp_listener.go
@@ -32,8 +32,6 @@ func main() {
 			os.Exit(1)
 		}
 
-		fmt.Println([]byte(string(buf[:n])))
-
 		conn.Write([]byte("+OK\r\n"))
 	}
 
