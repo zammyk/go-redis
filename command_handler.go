98bc0c37ba87f419c68c18b034af957df6a6d6aa
Rahul Hembrom <rhembrom21@gmail.com>
2024-07-01 22:28:01 +0530

    Add ping handler in command_handler

---
+++ command_handler.go
@@ -0,0 +1,12 @@
+package main
+
+var Handlers = map[string]func([]Value) Value{
+	"ping": ping,
+}
+
+func ping(args []Value) Value {
+	if len(args) == 0 {
+		return Value{typ: "string", str: "PONG"}
+	}
+	return Value{typ: "string", str: args[0].bulk}
+}
