00b8a48ecdd066d94af494bbdcb2bc7929c4f31a
Rahul Hembrom <rhembrom21@gmail.com>
2024-07-18 20:50:45 +0530

    Add COMMAND handler

--- command_handler.go
+++ command_handler.go
@@ -1,9 +1,10 @@
 package main
 
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-	"SET":  set,
-	"GET":  get,
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"COMMAND": ping,
 }
 
 func ping(args []Value) Value {
