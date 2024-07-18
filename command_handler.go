7fe24c603096cd93f4baf15d47ea6e27fffc91d8
Rahul Hembrom <rhembrom21@gmail.com>
2024-07-18 21:05:27 +0530

    Add HSET and HGET commands

--- command_handler.go
+++ command_handler.go
@@ -5,6 +5,8 @@ var Handlers = map[string]func([]Value) Value{
 	"SET":     set,
 	"GET":     get,
 	"COMMAND": ping,
+	"HSET":    hset,
+	"HGET":    hget,
 }
 
 func ping(args []Value) Value {
@@ -39,6 +41,44 @@ func get(args []Value) Value {
 	SETsMu.RLock()
 	value, ok := SETs[key]
 	SETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hsetKey := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
+	if _, ok := HSETs[hsetKey]; !ok {
+		HSETs[hsetKey] = map[string]string{}
+	}
+	HSETs[hsetKey][key] = value
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
+	}
+
+	hsetKey := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hsetKey][key]
+	HSETsMu.RUnlock()
 
 	if !ok {
 		return Value{typ: "null"}
